16113d2049106b9407eb233d17a25c9b1afb40b8
Martin Balk <m@martinbalk.com>
2018-10-30 16:11:56 +0100

    Refactor TimeLockable

--- contracts/lib/TimeLockable.sol
+++ contracts/lib/TimeLockable.sol
@@ -1,63 +1,50 @@
 pragma solidity 0.4.24;
 
 import "zos-lib/contracts/Initializable.sol";
-import "openzeppelin-eth/contracts/ownership/Ownable.sol";
 
-contract TimeLockable is Initializable, Ownable {
-    uint256 public releaseDate;
-    address public crowdsaleAddress;
+contract TimeLockable is Initializable {
+    uint256 private _unlockDate;
 
-    event CrowdsaleAddressSet(address _crowdsaleAddress);
+    event UnLockDateSet(address _caller, uint256 _newUnlockDate);
 
     /**
       * @dev Initializer
-      * @param _releaseDate The date from which users will be able to transfer
+      * @param _newUnlockDate The date from which users will be able to transfer
       * the token
       */
-    function initialize(uint _releaseDate, address _admin) public initializer {
-        require(_releaseDate > block.timestamp);
-        releaseDate = _releaseDate;
+    function initialize(uint256 _newUnlockDate) public initializer {
+        require(_newUnlockDate > block.timestamp);
 
-        Ownable.initialize(_admin);
+        _unlockDate = _newUnlockDate;
+
+        emit UnLockDateSet(msg.sender, _newUnlockDate);
     }
 
     /**
-      * @dev Reverts if are not past the releaseDate or if the caller isn't the owner
+      * @dev Allows possibile implementation in parent contract to edit the
+      * date at which users are allowed to trade their tokens
+      * @param _newUnlockDate The unlock date that will replace the former one
       */
-    modifier unlocked() {
-        require(block.timestamp > releaseDate || isOwner() || msg.sender == crowdsaleAddress);
-        _;
+    function _setUnlockDate(uint256 _newUnlockDate) internal {
+        require(block.timestamp < _unlockDate);
+        require(_newUnlockDate >= block.timestamp);
+
+        _unlockDate = _newUnlockDate;
+
+        emit UnLockDateSet(msg.sender, _newUnlockDate);
     }
 
     /**
-      * @dev Allows contract's owner to edit the date at which users are
-      * allowed to trade their token
-      * @param _newReleaseDate The release date that will replace the former one
+      * @return true if `_unlockDate` is in the future
       */
-    function setReleaseDate(uint256 _newReleaseDate) public onlyOwner {
-        require(block.timestamp < releaseDate);
-        require(_newReleaseDate >= block.timestamp);
-
-        releaseDate = _newReleaseDate;
+    function isTimeLocked() public view returns(bool) {
+        return block.timestamp < _unlockDate;
     }
 
     /**
-      * @dev NECESSARY: Allows contract's owner to set the address of the crowdsale's
-      * contract. This is necessary so the token contract allows the crowdsale contract
-      * to deal the tokens during lock-up period.
-      * @param _crowdsaleAddress The address of the crowdsale contract
+      * @return the address of the owner.
       */
-    function setCrowdsaleAddress(address _crowdsaleAddress) public onlyOwner {
-        crowdsaleAddress = _crowdsaleAddress;
-
-        emit CrowdsaleAddressSet(_crowdsaleAddress);
-    }
-
-    /**
-      * @dev Gives the crowdsaleAddress the token is bound to
-      * @return The current crowdsaleAddress
-      */
-    function getCrowdsaleAddress() public view returns(address) {
-        return crowdsaleAddress;
+    function unlockDate() public view returns(uint256) {
+        return _unlockDate;
     }
 }
